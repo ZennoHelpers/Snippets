3b777b7a4adf054b5920ed4812d821b6db763f6a
Meteorburn <flymount12@gmail.com>
2019-09-17 23:51:30 +0300

    Изменения в сниппетах

--- Сниппеты/[Списки]/Взять диапозон строк из файла и вернуть строку добавив разделитель.cs
+++ Сниппеты/[Списки]/Взять диапозон строк из файла и вернуть строку добавив разделитель.cs
@@ -1,4 +1,4 @@
-﻿// ВЗЯТЬ ДИАПОЗОН СТРОК ИЗ ФАЙЛА И ВЕРНУТЬ СТРОКУ ДОБАВИВ РАЗДЕЛИТЕЛЬ 
+// ВЗЯТЬ ДИАПОЗОН СТРОК ИЗ ФАЙЛА И ВЕРНУТЬ СТРОКУ ДОБАВИВ РАЗДЕЛИТЕЛЬ 
 
 // путь к файлу
 string path = project.Variables["pathToKeyWordFile"].Value;
@@ -14,16 +14,7 @@ string path = project.Variables["pathToKeyWordFile"].Value;
 */
 
 // создаём список всех строк
-var lines = new List<string>();
-
-// получаем количество всех строк
-int count = Convert.ToInt32(Macros.FileSystem.FileCountOfLines(path));
-
-// загрузим все строки в список
-for(int i = 0; i < count; i++)
-{
-	lines.Add(Macros.FileSystem.FileGetLine(path, i.ToString(), false));
-}
+var lines = File.ReadAllLines(path);
 
 // возвращаем ключевые слова через ";" в указанном диапазоне от 3 до 6
 // т.е. начиная с 2-го индекса в количестве 4 штуки
--- Сниппеты/[Списки]/Удаление из списка по Regex.cs
+++ Сниппеты/[Списки]/Удаление из списка по Regex.cs
@@ -1,4 +1,4 @@
-﻿// УДАЛЕНИЕ ИЗ СПИСКА ПО REGEX //
+// УДАЛЕНИЕ ИЗ СПИСКА ПО REGEX //
 
 // берем регулярное выражение для парсинга из переменной
 var parserRegexPattern = project.Variables["listSearchRegex"].Value;
@@ -13,8 +13,8 @@ lock(SyncObjects.ListSyncer)
 {
     for(int i = sourceList.Count-1; i >= 0; i--)
     {
-        // читаем строку из списка
-        var str = sourceList[i];
+		// читаем строку из списка
+		var str = sourceList[i];
         // проверяем строку регулярным выражением, если есть совпадение удаляем
         if (parserRegex.IsMatch(str))
 		{
