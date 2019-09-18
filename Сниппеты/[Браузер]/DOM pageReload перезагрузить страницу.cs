2bb12e0cb6fca8a24bb8c63cf6352f33c3af3f11
AZANIR <eleoneks@gmail.com>
2019-09-18 14:55:54 +0300

    DOM pageReload перезагрузить страницу
    //мягкая перезагрузка, F5
    instance.ActiveTab.MainDocument.EvaluateScript("window.location.reload(false)");
    
    //жесткая перезагрузка, как Ctrl+F5
    instance.ActiveTab.MainDocument.EvaluateScript("window.location.reload(true)");

---
+++ Сниппеты/[Браузер]/DOM pageReload перезагрузить страницу.cs
@@ -0,0 +1,5 @@
+//м€гка€ перезагрузка, F5
+instance.ActiveTab.MainDocument.EvaluateScript("window.location.reload(false)");
+
+//жЄстка€ перезагрузка, как Ctrl+F5
+instance.ActiveTab.MainDocument.EvaluateScript("window.location.reload(true)");
