1b6f98696b722430232d48ac3c4d407a8a194d27
Dmitry Kolomiets <k.dmitriy@dolphins.ru.com>
2019-09-18 20:57:27 +0300

    Update Генерация случайного англоязычного пароля.cs

--- Сниппеты/[Текст]/Генерация случайного англоязычного пароля.cs
+++ Сниппеты/[Текст]/Генерация случайного англоязычного пароля.cs
@@ -1,14 +1,4 @@
 int size = 8;
-bool lowerCase = false;  
-		
-StringBuilder builder = new StringBuilder();  
-Random random = new Random();  
-char ch;  
-for (int i = 0; i < size; i++)  
-{  
-    ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));  
-    builder.Append(ch);  
-}  
-if (lowerCase)  
-    return builder.ToString().ToLower();  
-return builder.ToString();  
+string alphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789";
+
+return ZennoLab.Macros.TextProcessing.RandomText(size, "s", alphabet);
