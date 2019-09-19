1ab09191738c6dd86ed3edc6ced01b8c8175f088
AZANIR <eleoneks@gmail.com>
2019-09-19 13:42:24 +0300

    Удалить пустые строки
    Удалить пустые строки в файле

---
+++ Сниппеты/[Текст]/Удаление пустых строк.cs
@@ -0,0 +1,9 @@
+// Delete blank lines from file
+// Удалить пустые строки в файле
+// Gets file path from your project variable with name "filePath"
+// Получаем путь к файлу из переменной "filePath"
+string path = project.Variables["filePath"].Value; 
+var lines = System.IO.File.ReadAllLines(path).Where(arg => !string.IsNullOrWhiteSpace(arg));
+System.IO.File.WriteAllLines(path, lines);
+return 0;
+ 
