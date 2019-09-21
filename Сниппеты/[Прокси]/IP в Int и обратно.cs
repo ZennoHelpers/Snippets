7f2cd671ae93f6b8ac7c5e4bbb6238e2aeffc7d9
BAZAg <aramic2007@gmail.com>
2019-09-21 07:03:13 +0300

    Исправил пробелы и название переменной

--- Сниппеты/[Прокси]/IP в Int и обратно.cs
+++ Сниппеты/[Прокси]/IP в Int и обратно.cs
@@ -18,25 +18,25 @@ project.SendInfoToLog(string.Format(@"uint={0} IP={1} ",int_ip, ip));
 
 // Добавляем в общий код
 public class CommonCode {
-	public static uint IpToInt (string ip){
-		string[] absd = ip.Trim().Split('.');
-			if( absd.Length == 4 ) {				
-				uint a = uint.Parse(absd[0]) * 256 * 256 * 256;
-				uint b = uint.Parse(absd[1]) * 256 * 256;
-				uint c = uint.Parse(absd[2]) * 256;
-				uint d = uint.Parse(absd[3]);
-				return a+b+c+d;
-	        }
-			else{
-				return 0;
-			}
-	}
-	public static string IntToIp (uint ip){
-		int z = (int) Math.Pow(2, Math.Pow(2, 3));
-		long a = ip/z/z/z;
-		long b = ( ip - a*z*z*z )/z/z;
-		long c = (( ip - a*z*z*z - b*z*z)/z);
-		long d = ( ip - a * z*z*z - b * z*z - c * z); 
-		return string.Format("{0}.{1}.{2}.{3}", a,b,c,d);
-	}
+    public static uint IpToInt (string ip){
+        string[] abcd = ip.Trim().Split('.');
+        if( abcd.Length == 4 ) {
+            uint a = uint.Parse(abcd[0]) * 256 * 256 * 256;
+            uint b = uint.Parse(abcd[1]) * 256 * 256;
+            uint c = uint.Parse(abcd[2]) * 256;
+            uint d = uint.Parse(abcd[3]);
+            return a+b+c+d;
+        }
+        else{
+            return 0;
+        }
+    }
+    public static string IntToIp (uint ip){
+        int z = (int) Math.Pow(2, Math.Pow(2, 3));
+        long a = ip/z/z/z;
+        long b = ( ip - a*z*z*z )/z/z;
+        long c = (( ip - a*z*z*z - b*z*z)/z);
+        long d = ( ip - a * z*z*z - b * z*z - c * z); 
+        return string.Format("{0}.{1}.{2}.{3}", a,b,c,d);
+    }
 }
