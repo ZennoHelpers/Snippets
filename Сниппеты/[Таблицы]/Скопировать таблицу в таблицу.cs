bf365f2b924efedadbef38f1d0f2aefe94677af9
Igor <specialist-seo@yandex.com>
2020-01-21 11:29:11 +0300

    Update Скопировать таблицу в таблицу.cs

--- Сниппеты/[Таблицы]/Скопировать таблицу в таблицу.cs
+++ Сниппеты/[Таблицы]/Скопировать таблицу в таблицу.cs
@@ -2,5 +2,5 @@
 var table1 = project.Tables["Таблица 1"];
 var table2 = project.Tables["Таблица 2"];
 for(int i=0; i<table1.RowCount; i++) {
-    table2.AddRow(table1[i]);
+    table2.AddRow(table1.GetRow(i).ToArray());
 }
