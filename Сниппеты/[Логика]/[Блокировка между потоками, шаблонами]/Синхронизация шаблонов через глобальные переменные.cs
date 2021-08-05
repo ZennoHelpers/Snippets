16e630589566ffb6ca11791978dca1dfc26fdbd9
Yuriy Zymlex <Zymlex@users.noreply.github.com>
2021-08-05 14:43:27 +0000

    Update Синхронизация шаблонов через глобальные переменные.cs

--- Сниппеты/[Логика]/[Блокировка между потоками, шаблонами]/Синхронизация шаблонов через глобальные переменные.cs
+++ Сниппеты/[Логика]/[Блокировка между потоками, шаблонами]/Синхронизация шаблонов через глобальные переменные.cs
@@ -1,19 +1,20 @@
 // Создание объектов синхронизации в глобальной переменной для изменения списков,
 // таблиц, других глобальных переменных и т.д. в разных шаблонах 
 
+// неймспейс и имя новой переменой для синхронизации
 string namespaceName = "test_namespace";
 string globVarName = "my_sync_object";
 
 // Выполняется в начале проекта
 lock (project.GlobalVariables)
 {
-	// проверка на существование этой глобальной переменной
+	// проверка на существование глобальной переменной
 	try {
 		// попытка получения объекта (переход на catch при неудаче)
 		var syncobj = project.GlobalVariables[namespaceName, globVarName];
 		return syncobj.ToString(); // возврат его значения (отобразится в логе PM, если возможно)
 	} catch (KeyNotFoundException ex) {
-		// создание объекта синхронизации и его установка, объект может быть любой
+		// создание объекта синхронизации и его установка, объект может быть любым: числом, строкой и т.д.
 		object syncobj = new Object();
 		project.GlobalVariables.SetVariable(namespaceName, globVarName, syncobj);
 	}
